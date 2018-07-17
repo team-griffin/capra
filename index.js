f629cd1b892455ec16c6a22e273bd01656297c96
Jack <jack.m.ellis@hotmail.co.uk>
2018-07-17 09:52:08 +0100

    added some more methods

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ export const hasLength = r.pipe(
   r.length,
   r.gt(r.__, 0),
 );
+
+export const isNotNilOrEmpty = r.both(
+  r.complement(r.isNil),
+  r.complement(r.isEmpty),
+);
+
+export const mapIndexed = r.addIndex(r.map);
+
+export const allTrue = r.all(isTrue);
+
+export const allFalse = r.all(isFalse);
+
+export const anyTrue = r.any(isTrue);
+
+export const ifElseBool = r.ifElse(isTrue);
